eaa2d01c20496e0bd44da2c3acd1998c0b8e3d23
Michael Vance <mexvance04@gmail.com>
2019-04-10 12:11:17 -0600

    Got sqlite to work :) found bugs and updated them on pages

--- CMSWebPageCreator/Migrations/20190410180659_sqlite.Designer.cs
+++ CMSWebPageCreator/Migrations/20190410180659_sqlite.Designer.cs
@@ -3,23 +3,20 @@ using System;
 using CMSWebPageCreator.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CMSWebPageCreator.Migrations
 {
     [DbContext(typeof(DBContext))]
-    [Migration("20190410161837_SQlite")]
-    partial class SQlite
+    [Migration("20190410180659_sqlite")]
+    partial class sqlite
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.1-servicing-10028")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
 
             modelBuilder.Entity("CMSWebPageCreator.Models.BodyInfo", b =>
                 {
--- CMSWebPageCreator/Migrations/DBContextModelSnapshot.cs
+++ CMSWebPageCreator/Migrations/DBContextModelSnapshot.cs
@@ -3,7 +3,6 @@ using System;
 using CMSWebPageCreator.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CMSWebPageCreator.Migrations
@@ -15,9 +14,7 @@ namespace CMSWebPageCreator.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.1-servicing-10028")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
 
             modelBuilder.Entity("CMSWebPageCreator.Models.BodyInfo", b =>
                 {
