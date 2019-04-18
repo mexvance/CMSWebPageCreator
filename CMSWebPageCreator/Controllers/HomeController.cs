265550ed1481998f11afcca6e5a6a0567739bc94
Michael Vance <mexvance04@gmail.com>
2019-04-18 10:26:22 -0600

    Added a quote of the day rest api to the home page

--- CMSWebPageCreator/Controllers/HomeController.cs
+++ CMSWebPageCreator/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CMSWebPageCreator.Models;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using RestSharp;
 
 namespace CMSWebPageCreator.Controllers
 {
@@ -12,7 +15,20 @@ namespace CMSWebPageCreator.Controllers
     {
         public IActionResult Index()
         {
+            //var request1 = GetReleases("https://theysaidso.com/api");
+
+            var client = new RestClient(" http://quotes.rest/");
+            var request = new RestRequest("qod", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            var jObject = JObject.Parse(content);
+            var myQuote = (string)jObject["contents"]["quotes"][0]["quote"];
+            var myQuoteAuthor = (string)jObject["contents"]["quotes"][0]["author"];
+            ViewData["quote"] = myQuote;
+            ViewData["author"] = myQuoteAuthor;
             return View();
+
         }
 
         public IActionResult Privacy()
@@ -24,6 +40,16 @@ namespace CMSWebPageCreator.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string GetReleases(string url)
+        {
+            var client = new WebClient();
+            //client.Headers.Add(RequestConstants.UserAgent, RequestConstants.UserAgentValue);
+
+            var response = client.DownloadString(url);
+
+            return response;
         }
     }
 }
