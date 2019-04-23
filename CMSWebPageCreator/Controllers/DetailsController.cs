7808bb7b3d49353226524fbf019f853b394092e5
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2019-04-23 10:21:25 -0600

    Shows number of pages in db

--- CMSWebPageCreator/Controllers/DetailsController.cs
+++ CMSWebPageCreator/Controllers/DetailsController.cs
@@ -2,20 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMSWebPageCreator.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CMSWebPageCreator.Controllers
 {
     public class DetailsController : Controller
     {
+        private readonly DBContext dBContext;
+
+        public DetailsController(DBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult DetailDB()
         {
-            return View();
+            var pageCount=dBContext.PageCreate.Count();
+            var numOfPageWithALetter = dBContext.PageCreate.Count();
+            var detailPageVM = new DbDetailViewModel();
+         
+            detailPageVM.PageCount = pageCount;
+
+            return View(detailPageVM);
         }
+
+        
 
 
     }
---
+++ CMSWebPageCreator/Models/DbDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSWebPageCreator.Models
+{
+    public class DbDetailViewModel
+    {
+        public int PageCount { get; set; }
+    }
+}
